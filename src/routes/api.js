d65711e7b7c4465e7893526caba880d7143dd365
longph <longphamduy2002@gmail.com>
2023-02-06 19:29:29 +0200

    update params checking

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const {quotes, fetchQuotes} = require('../../assets/quotes');
+const { quotes, fetchQuotes } = require("../../assets/quotes");
+const FETCH_LIMIT = 10;
 //main route
 router.get("/quotes", async (req, res) => {
-    res.json(fetchQuotes(1));
+  const quote = fetchQuotes(1);
+  if (quote) return res.status(200).json(quote);
 });
 
 //fetching report route
 router.get("/quotes/:amount", async (req, res) => {
-    const requested_amount = Number(req.params.amount);
-    isNaN(requested_amount) ? res.statusCode = 500 : res.statusCode = 200;
-    res.json(fetchQuotes(requested_amount));
+  const requested_amount = Number(req.params.amount);
+  if (isNaN(requested_amount)) {
+    return res.status(400).send("Fetch limit has to a number!");
+  } else {
+    if (requested_amount <= FETCH_LIMIT) {
+      const quotes = fetchQuotes(requested_amount);
+      return res.status(200).json(quotes);
+    } else return res.status(400).send("Fetch limit is 10 quotes per request!");
+  }
 });
 
 //export router
