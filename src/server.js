114bf0d8af022155f53c470793b0ac35022f1170
longph <longphamduy2002@gmail.com>
2023-02-06 19:29:38 +0200

    set universal cache

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const api = require('./routes/api');
 const app = express();
 
+app.all('*', function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use('/api', api);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => { console.log("Server running on port ", PORT); });
+app.listen(PORT, () => { console.log( `Server running on port ${PORT}`  ); });
