d072e86625a973ae6283abd3b23bc3ceebcef7be
longph <longphamduy2002@gmail.com>
2023-02-07 01:03:01 +0200

    add util middlewares

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const path = require('path');
+const helmet = require("helmet");
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
 const api = require('./routes/api');
 const app = express();
 
+
+const apiLimiter = rateLimit({
+	windowMs: 24* 60 * 60 * 1000, // 1 day in ms
+	max: 10000, 
+	standardHeaders: true, 
+	legacyHeaders: false,
+})
+
+app.use('/api/', apiLimiter);
+app.use(helmet());
+app.use(compression());
+app.set('view engine', 'ejs');
+app.set("views", (path.join(__dirname, "public", "views")));
+app.use(express.static(path.join(__dirname, "public" ,"assets")));
 app.all('*', function (req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
     next();
@@ -11,9 +29,9 @@ app.use('/api', api);
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    res.send("AHA!");
-})
+    res.render('home');
+});
 
-const PORT = process.env.PORT || 9002;
+const PORT = process.env.PORT || 5173;
 
 app.listen(PORT, () => { console.log( `Server running on port ${PORT}`  ); });
