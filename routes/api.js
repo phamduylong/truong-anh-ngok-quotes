86b94b8d536f384c6310be0b6d285b9c1a3f6f20
longph <longphamduy2002@gmail.com>
2023-02-10 21:27:53 +0200

    add search route

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { fetchQuotes, fetchAllQuotes } = require("../public/assets/quotes");
+const { fetchQuotes, fetchAllQuotes, searchQuotes } = require("../public/assets/quotes");
 const FETCH_LIMIT = 10;
 //main route
 router.get("/quotes", async (req, res) => {
@@ -22,11 +22,19 @@ router.get("/quotes/:amount", async (req, res) => {
   } else {
     if (requested_amount <= FETCH_LIMIT && requested_amount > 0) {
       const quotes = fetchQuotes(requested_amount);
-      const return_obj = {status: 200, data: quotes}
+      const return_obj = {status: 200, data: quotes};
       return res.status(200).json(return_obj);
     } else return res.status(400).json({status: 400, error: "Fetch amount has to be larger than 0 and less than or equal to 10!"});
   }
 });
+
+router.get("/quotes/search/:query?", async (req, res) => {
+  const query = req.params.query;
+  if(query) {
+    return res.status(200).json(searchQuotes(query));
+  }
+  return res.status(400).json({status: 400, error: "Invalid search query!"});
+})
 
 //export router
 module.exports = router;
