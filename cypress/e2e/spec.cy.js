156f9cef8dac5fb4ea7dba79ae841b58688c367c
longph <longphamduy2002@gmail.com>
2023-02-14 21:47:37 +0200

    add some anchor tag test cases

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,40 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+describe("Page Contain Title", () => {
+  it("Correct Title", () => {
+    cy.visit("https://blv-anh-ngok-said.onrender.com/");
+    cy.contains("BLV ANH NGOK");
+  });
+});
+
+describe("Landing Page Anchors", () => {
+  it("Should Contain 8 Anchors", () => {
+    cy.visit("https://blv-anh-ngok-said.onrender.com/");
+    cy.get("a")
+      .should("not.be.empty")
+      .and(($a) => {
+        // should have found 8 elements
+        expect($a).to.have.length(8);
+      });
+  });
+
+  it("Should Contain 5 API Anchors", () => {
+    cy.visit("http://localhost:5173");
+    cy.get(".api-link")
+      .should("not.be.empty")
+      .and(($a) => {
+        // should have found 5 elements
+        expect($a).to.have.length(5);
+
+        const hrefs = $a.map((idx, elem) => {
+          return Cypress.$(elem).attr('href');
+        });
+
+        expect(hrefs.get()).to.deep.eq([
+          "/api/quotes",
+          "/api/quotes/3",
+          "/api/quotes/15",
+          "/api/quotes/search/em",
+          "/api/quotes/search/"
+        ]);
+      });
+  });
+});
