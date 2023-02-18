eeb90d114089ccbfb1a88b55454382567b361a65
longph <longphamduy2002@gmail.com>
2023-02-18 19:45:24 +0200

    use beforeeach hook

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,38 +1,37 @@
+
+beforeEach(() => {
+  cy.visit("/");
+});
+
 describe("Page Contain Title", () => {
   it("Correct Title", () => {
-    cy.visit("/");
     cy.title().should("be.deep.eq", "Anh Ngok's API");
   });
 });
 
 describe("Page Contain Banner", () => {
   it("Correct Banner", () => {
-    cy.visit("/");
     cy.get("#banner").should("have.text", "BLV ANH NGOK'S API 🐰");
   });
 });
 
 describe("Landing Page Anchors", () => {
   it("Should Contain 8 Anchors", () => {
-    cy.visit("/");
     cy.get("a")
       .should("not.be.empty")
       .and(($a) => {
-        // should have found 8 elements
         expect($a).to.have.length(8);
       });
   });
 
   it("Should Contain 5 API Anchors", () => {
-    cy.visit("/");
     cy.get(".api-link")
       .should("not.be.empty")
       .and(($a) => {
-        // should have found 5 elements
         expect($a).to.have.length(5);
 
         const hrefs = $a.map((idx, elem) => {
-          return Cypress.$(elem).attr('href');
+          return Cypress.$(elem).attr("href");
         });
 
         expect(hrefs.get()).to.deep.eq([
@@ -40,39 +39,32 @@ describe("Landing Page Anchors", () => {
           "/api/quotes/3",
           "/api/quotes/15",
           "/api/quotes/search/em",
-          "/api/quotes/search/"
+          "/api/quotes/search/",
         ]);
       });
   });
 });
 
 describe("Code Boxes Should Not Be Empty", () => {
-
   it("Should Contain 5 Code Boxes", () => {
-    cy.visit("/");
     cy.get("code")
       .should("not.be.empty")
       .and(($c) => {
-        // should have found 5 elements
         expect($c).to.have.length(5);
-
       });
   });
 
-
   it("Code Boxes Should Never Be Empty", () => {
-    cy.visit("/");
     cy.get("code")
       .should("not.be.empty")
       .and(($c) => {
-        const boxes_content = $c.map(box => {
+        const boxes_content = $c.map((box) => {
           return Cypress.$(box).innerText;
         });
 
-        boxes_content.get().forEach(box => {
+        boxes_content.get().forEach((box) => {
           expect(box).to.not.be.empty();
-        })
-
+        });
       });
   });
-})
+});
